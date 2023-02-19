df2466293d72cae70c6a68ec1851b23ad567a8a8
Xu Han <xu.han@outlook.com>
2023-02-19 10:15:05 +0800

    add malloc and access test.

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,46 @@
 
 #define COUNT 1000*1000
 
-void bench_func()
+void bench_malloc_func()
 {
 	for (size_t i = 0; i < COUNT; ++i)
 	{
-		int * pInt = (int*)malloc(i * sizeof(int));
+		int malloc_size = i * sizeof(int);
+		int * pInt = (int*)malloc(malloc_size);
 		free(pInt);
 	}
 }
+
+void bench_malloc_and_access_func()
+{
+	for (size_t i = 0; i < COUNT; ++i)
+	{
+		int malloc_size = i * sizeof(int);
+		int * pInt = (int*)malloc(malloc_size);
+
+		memset(pInt, 0, malloc_size);
+		free(pInt);
+	}
+}
+
 
 int main()
 {
   using namespace std::chrono;
 
   high_resolution_clock::time_point time_begin = high_resolution_clock::now();
-
-  bench_func();
-
+  bench_malloc_func();
   high_resolution_clock::time_point time_end = high_resolution_clock::now();
   duration<double> time_span = duration_cast<duration<double>>(time_end - time_begin);
 
-  printf_s("duration time: %lf.\n", time_span.count());
+  printf_s("bench malloc duration time: %lfs.\n", time_span.count());
+
+  time_begin = high_resolution_clock::now();
+  bench_malloc_and_access_func();
+  time_end = high_resolution_clock::now();
+  time_span = duration_cast<duration<double>>(time_end - time_begin);
+
+  printf_s("bench malloc and access duration time: %lfs.\n", time_span.count());
 
   return 0;
 }
